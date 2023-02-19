912a0f92687a6c4d8f5d84e1e117617f0c952c43
aguadagn <aguadjr@gmail.com>
2023-02-18 23:45:54 -0500

    updated web scraper

--- WebScraperTest.py
+++ WebScraperTest.py
@@ -1,28 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
+# user inputs song and the input is split up
 input_song = input("What song would you like to create a playlist based off of? Enter your response in the 'song name-artist name' format: ")
 song_name = input_song.split("-")[0]
 artist_name = input_song.split("-")[1]
 
-artist_name = artist_name.title()
-
-print(song_name)
-print(artist_name)
-
+# test songs:
 # aerials-system of a down
+# master of puppets-metallica
+# lateralus-tool
 
-musicstax_url = f"https://musicstax.com/search?q={song_name}+{artist_name}"
-
+# find the musicstax page for input song and parse it
+musicstax_url = f"https://musicstax.com/search?q={input_song}"
 response = requests.get(musicstax_url)
 song_scrape = response.text
-
 soup = BeautifulSoup(song_scrape, "html.parser")
 
-div_tag = soup.find('div', class_='song-artist')
-a_tag = div_tag.find_parent('a', class_='song-details-right')
+# find the 'a' tag with the class 'song-details-right'
+a_tag = soup.find('a', class_='song-details-right')
+
+# get the link within the 'a' tag we found and print it
 link = a_tag.get('href')
 print(link)
 
-digits = link.split("/")[3]
-print(digits)
+# split the link we found up so that we get the song name and string at the end and print it
+track = link.split("track")[1]
+print(track)
+
+# find the musicstax page for similar songs to the track we input
+musicstax_url2 = f"https://musicstax.com/track/similar{track}"
+response2 = requests.get(musicstax_url2)
+song_scrape2 = response2.text
+soup2 = BeautifulSoup(song_scrape2, "html.parser")
+
+# find all links under the 'a' tag with the class artist-seed-track-right
+for link in soup2.find_all('a', class_='artist-seed-track-right'):
+    similar_track = link.get('href')
+    similar_track_name = similar_track.split("/")[2]
+    print(similar_track_name)
